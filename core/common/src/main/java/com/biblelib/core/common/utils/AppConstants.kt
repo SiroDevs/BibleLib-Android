5b060e0c77b70467a76466e118b5dd231e4ea6c8
Siro Devs <sirodevske@gmail.com>
2026-09-17 19:48:51 +0300

    api fixes

--- core/common/src/main/java/com/biblelib/core/common/utils/AppConstants.kt
+++ core/common/src/main/java/com/biblelib/core/common/utils/AppConstants.kt
@@ -10,7 +10,7 @@ object ApiConstants {
     const val PAYSTACK_CALLBACK_URL = "https://biblive.vercel.app/donation/callback"
     const val DONOR_EMAIL = "anonymous_donor@biblelib.app"
 
-    const val BIBLELIB_BASE = "https://biblive.vercel.app/"
+    const val BIBLELIB_BASE = "https://biblive.vercel.app/v2/"
 }
 
 object PayConstants {
